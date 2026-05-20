71246a9a7604d627d9f88f4080f78f4a988a17e7
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2026-05-20 19:51:19 +0530

    fixed bug

--- check_result.py
+++ check_result.py
@@ -25,8 +25,6 @@ def send_telegram_message(message):
         print("Telegram notification sent")
     else:
         print("Telegram notification failed")
-        print("Telegram notification failed")
-        print(response.text)
 
 try:
     response = requests.get(URL, timeout=20)
