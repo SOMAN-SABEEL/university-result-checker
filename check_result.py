db445300c1b49c83e3d4c6fe6d222c0612e2bc7b
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2026-05-20 19:27:54 +0530

    uploading files to git

---
+++ check_result.py
@@ -0,0 +1,63 @@
+import requests
+from bs4 import BeautifulSoup
+import os
+
+URL = "https://egov.uok.edu.in/results/"
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+KEYWORDS = [
+    "b.tech 1st semester",
+    "b.tech 1st sem",
+    "regular batch 2025"
+]
+
+def send_telegram_message(message):
+    telegram_url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    data = {
+        "chat_id": CHAT_ID,
+        "text": message
+    }
+
+    response = requests.post(telegram_url, data=data)
+
+    if response.status_code == 200:
+        print("Telegram notification sent")
+    else:
+        print("Telegram notification failed")
+
+try:
+    response = requests.get(URL, timeout=20)
+
+    if response.status_code != 200:
+        print(f"Website error: {response.status_code}")
+        exit()
+
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    page_text = soup.get_text().lower()
+
+    found_keywords = []
+
+    for keyword in KEYWORDS:
+        if keyword.lower() in page_text:
+            found_keywords.append(keyword)
+
+    if found_keywords:
+        message = (
+            "1st Semester Result Notification May Be Out\n\n"
+            "Detected Keywords:\n- "
+            + "\n- ".join(found_keywords)
+        )
+
+        print(message)
+
+        send_telegram_message(message)
+
+    else:
+        print("No result notification found yet.")
+
+except Exception as e:
+    print(f"Error: {e}")
